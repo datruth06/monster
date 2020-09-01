6dc495044493527b8a6101836cb9cbab5f1d20b1
Allen C <dadarksideofdamoon@yahoo.com>
2020-09-01 12:03:42 -0400

    Updates

---
+++ precache-manifest.393484719987fc0a15cfe6a0966f46e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71f9a2f3c4834f0da1d405a317f16cf6",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "8498876cedf24a8ddef5",
+    "url": "/monster/static/css/main.725b83d4.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/monster/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8498876cedf24a8ddef5",
+    "url": "/monster/static/js/main.9cfc20c9.chunk.js"
+  },
+  {
+    "revision": "2de6668e104244d15238",
+    "url": "/monster/static/js/runtime-main.b327a331.js"
+  }
+]);
